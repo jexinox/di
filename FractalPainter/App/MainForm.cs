987c1df498fb8ef757a53b2cad16ec7a1c5312d5
jexinox <jexinox@gmail.com>
2022-11-29 18:50:34 +0500

    added first 3 readme tasks solve

--- FractalPainter/App/MainForm.cs
+++ FractalPainter/App/MainForm.cs
@@ -11,20 +11,7 @@ namespace FractalPainting.App
 {
     public class MainForm : Form
     {
-        public MainForm()
-            : this(
-                new IUiAction[]
-                {
-                    new SaveImageAction(),
-                    new DragonFractalAction(),
-                    new KochFractalAction(),
-                    new ImageSettingsAction(),
-                    new PaletteSettingsAction()
-                })
-        {
-        }
-
-        public MainForm(IUiAction[] actions)
+        public MainForm(IUiAction[] actions, PictureBoxImageHolder pictureBoxImageHolder, Palette palette)
         {
             var imageSettings = CreateSettingsManager().Load().ImageSettings;
             ClientSize = new Size(imageSettings.Width, imageSettings.Height);
@@ -32,16 +19,15 @@ namespace FractalPainting.App
             var mainMenu = new MenuStrip();
             mainMenu.Items.AddRange(actions.ToMenuItems());
             Controls.Add(mainMenu);
+            
+            pictureBoxImageHolder.RecreateImage(imageSettings);
+            pictureBoxImageHolder.Dock = DockStyle.Fill;
+            Controls.Add(pictureBoxImageHolder);
 
-            var pictureBox = new PictureBoxImageHolder();
-            pictureBox.RecreateImage(imageSettings);
-            pictureBox.Dock = DockStyle.Fill;
-            Controls.Add(pictureBox);
-
-            DependencyInjector.Inject<IImageHolder>(actions, pictureBox);
+            DependencyInjector.Inject<IImageHolder>(actions, pictureBoxImageHolder);
             DependencyInjector.Inject<IImageDirectoryProvider>(actions, CreateSettingsManager().Load());
             DependencyInjector.Inject<IImageSettingsProvider>(actions, CreateSettingsManager().Load());
-            DependencyInjector.Inject(actions, new Palette());
+            DependencyInjector.Inject(actions, palette);
         }
 
         private static SettingsManager CreateSettingsManager()
--- FractalPainter/App/Program.cs
+++ FractalPainter/App/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Windows.Forms;
+using FractalPainting.App.Actions;
+using FractalPainting.Infrastructure.Common;
+using FractalPainting.Infrastructure.UiActions;
 using Ninject;
 
 namespace FractalPainting.App
@@ -18,10 +21,21 @@ namespace FractalPainting.App
 
                 // start here
                 // container.Bind<TService>().To<TImplementation>();
+                container.Bind<Palette>().ToSelf().InSingletonScope();
+                container.Bind<PictureBoxImageHolder>().ToSelf().InSingletonScope();
 
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new MainForm());
+                Application.Run(new MainForm(new IUiAction[]
+                {
+                    new SaveImageAction(),
+                    new DragonFractalAction(),
+                    new KochFractalAction(container.Get<PictureBoxImageHolder>(), container.Get<Palette>()),
+                    new ImageSettingsAction(),
+                    new PaletteSettingsAction()
+                }, 
+                    container.Get<PictureBoxImageHolder>(),
+                    container.Get<Palette>()));
             }
             catch (Exception e)
             {
